fd7df5574d2dbc0cee78428d488dcae0cfcb929e
shifaniyacs92 <shifaniyacs92@gmail.com>
2025-10-08 20:07:34 +0530

    update Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello from Flask CI/CD!"
+
+if __name__ == '__main__':
+    print("Starting Flask app...")
+    app.run(host='0.0.0.0', port=5000)
