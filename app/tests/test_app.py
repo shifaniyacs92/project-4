5d21c40f4f60abbd5e81888240b0e761db53f3a2
shifaniyacs92 <shifaniyacs92@gmail.com>
2025-10-08 13:45:25 +0530

    update app filename

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello, my World!" in response.data
+    assert b"Hello from Flask CI/CD!" in response.data
